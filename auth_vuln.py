e97de6446c136d8fe9ebe5f40ad325fddfc6b8cd
albertsjohnson <91748774+albertsjohnson@users.noreply.github.com>
2022-07-04 18:11:26 +0800

    Add files via upload

--- auth_vuln.py
+++ auth_vuln.py
@@ -1,25 +1,33 @@
 import os
 import re
 import requests
+import time
 
-url = 'http://www.tecnicarobertorocca.edu.ar/wp-json/'
+base_url = input("Input URL:    ")
+base_dir = input("Store Path:   ")
+if not os.path.exists(base_dir):
+    os.mkdir(base_dir)
+last_three_chars = base_url[-2:]
+
+url = f'http://{base_url}/wp-json/'
+
 headers = {
-'Origin':'https://www.haha.com',
+'Origin':'https://www.hellokit.com',
 'Cookie':'wordpress_test_cookie=WP%20Cookie%20check',
 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 'Accept-Encoding':'gzip,deflate',
 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-'Host':'www.tecnicarobertorocca.edu.ar',
+'Host':base_url,
 'Connection':'Keep-alive',
 }
 res = requests.get(url, headers=headers)
 print(res.headers)
-file = open("wp-json.api", mode="w+", encoding="utf-8")
+file = open(f"{base_dir}/wp-json.api", mode="w+", encoding="utf-8")
 file.write(res.text)
 file.flush()
 file.close()
 
-results = re.findall('http:.*?"', res.text)
+results = re.findall('http.*?"', res.text)
 index = 0
 
 results.sort()
@@ -31,9 +39,10 @@ for result in results:
     result = result.replace('\\', '');
     result = result[:-1]
     print(result)
-    if result.find('ar/') <= 0:
+    if result.find(f'{last_three_chars}/') <= 0:
         continue
-    dir_path = result[result.index('ar/')+3:result.rindex('/')]
+    dir_path = result[result.index(f'{last_three_chars}/')+3:result.rindex('/')]
+    dir_path = base_dir + "/" + dir_path
     print(dir_path)
     if os.path.isfile(dir_path):
         os.rename(dir_path, dir_path + '.txt')
@@ -47,7 +56,8 @@ for result in results:
     #print(result[-1:-1])
     if result[-1:] == '/':
         result += 'backslash.api'
-    file_path = result[result.index('ar/')+3:]
+    file_path = result[result.index(f'{last_three_chars}/')+3:]
+    file_path = base_dir + "/" + file_path
     if os.path.exists(file_path) and os.path.isdir(file_path):
         file_path += ".txt"
     file = open(file_path + '.cors', mode="w+", encoding="utf-8")
